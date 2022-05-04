cdb2dfdb9af20440aaea5694b9510b91aedc6d9e
kevinbert <keeew1992@gmail.com>
2022-05-04 14:50:55 +0200

    Added metods and CSS/HTML, for when u have answerd atleast one quiestion on the guided registration

--- IAT2022/ViewModels/RegisterProjectViewModel.cs
+++ IAT2022/ViewModels/RegisterProjectViewModel.cs
@@ -47,7 +47,6 @@ namespace IAT2022.ViewModels
         }
         public bool StepOne(ProjectPoco projectPoco)
         {
-
             var customer = projectPoco.Customer.Where(x => x.Result == true).ToList();
             var product = projectPoco.Product.Where(x => x.Result == true).ToList();
             if (customer.Count > 0 && product.Count > 0)
@@ -73,6 +72,60 @@ namespace IAT2022.ViewModels
                 return false;
             }
         }
+        public bool CheckIfAnswerd(ProjectPoco projectPoco, string input)
+        {
+            if (input == "Customer")
+            {
+                var customer = projectPoco.Customer.Where(x => x.Result == true).ToList();
+                if (customer.Count > 0)
+                {
+                    return true;
+                }
+            }
+            if (input == "Product")
+            {
+                var product = projectPoco.Product.Where(x => x.Result == true).ToList();
+                if (product.Count > 0)
+                {
+                    return true;
+                }
+            }
+            if (input == "IPR")
+            {
+                var Ipr = projectPoco.IPR.Where(x => x.Result == true).ToList();
+                if (Ipr.Count > 0)
+                {
+                    return true;
+                }
+            }
+            if (input == "Business")
+            {
+                var business = projectPoco.Business.Where(x => x.Result == true).ToList();
+                if (business.Count > 0)
+                {
+                    return true;
+                }
+            }
+            if (input == "Team")
+            {
+                var team = projectPoco.Team.Where(x => x.Result == true).ToList();
+                if (team.Count > 0)
+                {
+                    return true;
+                }
+            }
+            if (input == "Finance")
+            {
+                var finance = projectPoco.Finance.Where(x => x.Result == true).ToList();
+                if (finance.Count > 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+            
+        }
+
 
     }
 }
